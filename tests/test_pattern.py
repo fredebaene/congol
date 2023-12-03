31a84bc440c27dba5a08fe3175499d44d7a592c4
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-04 00:19:58 +0100

    Update unit tests

--- tests/test_pattern.py
+++ tests/test_pattern.py
@@ -2,11 +2,6 @@ import pytest
 from congol.pattern import Pattern
 
 
-@pytest.fixture
-def blinker_pattern():
-    return Pattern("Blinker", {(2, 1), (2, 2), (2, 3)})
-
-
 def test_pattern_initialization(blinker_pattern):
     assert blinker_pattern.name == "Blinker"
     assert set(sorted(blinker_pattern.alive_cells)) == {(2, 1), (2, 2), (2, 3)}
