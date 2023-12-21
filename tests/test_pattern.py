4255b1056c8bd6a924a6bde2d1d9e17813c42c3b
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-21 14:18:45 +0100

    Add test for reader function

--- tests/test_pattern.py
+++ tests/test_pattern.py
@@ -11,3 +11,9 @@ def _PATTERNS_FILE():
 def test_pattern_initialization(blinker_pattern):
     assert blinker_pattern.name == "Blinker"
     assert set(sorted(blinker_pattern.alive_cells)) == {(2, 1), (2, 2), (2, 3)}
+
+
+def test_reading_patterns_from_toml(_PATTERNS_FILE):
+    expected_res = {"Blinker": {"alive_cells": [[2, 1], [2, 2], [2, 3]]}}
+    observed_res = Pattern.read_patterns_from_toml(_PATTERNS_FILE)
+    assert expected_res == observed_res
