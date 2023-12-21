4f094db9c58fedd04eeb0e364bf7644350b77467
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-21 15:07:08 +0100

    Break code

--- tests/test_pattern.py
+++ tests/test_pattern.py
@@ -9,7 +9,7 @@ def _PATTERNS_FILE():
 
 
 def test_pattern_initialization(blinker_pattern):
-    assert blinker_pattern.name == "Blinker"
+    assert blinker_pattern.name == "blinker"
     assert set(sorted(blinker_pattern.alive_cells)) == set(sorted({(2, 1), (2, 2), (2, 3)}))
 
 
