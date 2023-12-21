9ee66ebd8a71ce60a026033dc1c44f9b7b047e8d
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-21 14:18:20 +0100

    Add fixture for patterns file

--- tests/test_pattern.py
+++ tests/test_pattern.py
@@ -3,6 +3,11 @@ import pytest
 from congol.pattern import Pattern
 
 
+@pytest.fixture
+def _PATTERNS_FILE():
+    return Path(__file__).parent.parent / "data" / "patterns.toml"
+
+
 def test_pattern_initialization(blinker_pattern):
     assert blinker_pattern.name == "Blinker"
     assert set(sorted(blinker_pattern.alive_cells)) == {(2, 1), (2, 2), (2, 3)}
